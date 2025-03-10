c557541bd9aa91e91e88c63c452d4a0c88ad35b2
DamianOyarceVVH <damianoyarce@liceovvh.cl>
2025-03-10 11:15:22 -0300

    1.3

---
+++ JS/03_EjerciciosVariables/script.js
@@ -0,0 +1,20 @@
+let numero = 0; // Numérica
+let decimal = 2.5; // Decimal (num float)
+let texto = "Hola"; // Texto
+let booleano = true; // Booleano
+let array = [1, "Hola", 25]; // Lista de elementos
+let matriz = [[1, 2], [3, 4]]; // Lista de listas
+let objeto = { // Conjunto de propiedades
+    nombre: "Damián",
+    edad: 17
+};
+
+function decimal() {
+    alert("5.5");
+}
+
+
+
+function sumar(a, b) { // Funcion que suma dos números
+    return a + b;
+};
