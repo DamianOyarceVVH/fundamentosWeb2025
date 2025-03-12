c430c77a598f5ea2eb3ca68b830bb1d5281e07b8
DamianOyarceVVH <damianoyarce@liceovvh.cl>
2025-03-12 09:30:39 -0300

    fix

--- JS/04_Ejercicios/script.js
+++ JS/04_Ejercicios/script.js
@@ -4,16 +4,15 @@ console.log("Vinculado correctamente...")
 // Luego, calcula y muestra en un mensaje la edad actual basada en el año de nacimiento.
 
 function declararVariables() {
-    let ahora = 2025; // Año actual
-    let fecha_nac = 2007; // Año de nacimiento
-    let edad = ahora - fecha_nac; // Cálculo
-    alert("La edad actual es: " + edad);
+    let ahora = parseInt(prompt("Ingresa el año actual: ")); // Año actual
+    let fecha_nac = parseInt(prompt("Ingresa el año de nacimiento: ")); // Año de nacimiento
+    alert("Tu edad es: " + (ahora - fecha_nac)); // Resta de años
 }
 
 // Ejercicio 2: Asignación de valores
 // Instrucciones: Completa la función para asignar el valor de 'var1' a 'var2' y luego muestra el valor de 'var2'.
 function asignarValores() {
-    let var1 = 42; // Valor
+    let var1 = prompt("Ingresa un valor: "); // Valor
     let var2 = var1; // Asignación de var1 a var2
     alert("El valor de var2 es: " + var2);
 }
@@ -23,24 +22,24 @@ function asignarValores() {
 // Suma 'num1','num2' y 'num3', resta 'num1' de 'num3', multiplica 'num2' por 'num3', y divide 'num1' entre '120'.
 // Muestra los resultados en un mensaje.
 function realizarOperaciones() {
-    let num1 = 162;
-    let num2 = 88;
-    let num3 = 66;
+    let num1 = parseInt(prompt("Ingresa el 1er valor: "));
+    let num2 = parseInt(prompt("Ingresa el 2do valor: "));
+    let num3 = parseInt(prompt("Ingresa el 3er valor: "));
     let suma = num1 + num2 + num3; // Suma
     let resta = num3 - num1; // Resta
     let multiplicacion = num2 * num3; // Multiplicación
     let division = num1 / 120; // División
-    alert("Resultados:\nSuma: " + suma + "\nResta: " + resta + "\nMultiplicación: " + multiplicacion + "\nDivisión: " + division);
+    alert("Resultados:\nSuma (" + num1 + " + " + num2 + " + " + num3 + "): " + suma + "\nResta (" + num3 + " - " + num1 + "): " + resta + "\nMultiplicación (" + num2 + " * " + num3 + "): " + multiplicacion + "\nDivisión (" + num1 + " / " + "120" + "): " + division);
 }
 
 // Ejercicio 4: Manipulación de cadenas
 // Instrucciones: Completa la función para construir un mensaje utilizando las variables 'direccion' y 'n_casa'.
 // Luego, muestra la longitud del mensaje y el antepenultimo en el mensaje. (.length)
 function manipularCadenas() {
-    let direccion = "Av. Observatorio";
-    let n_casa = 10962;
-    let mensaje = "Dirección: " + direccion + ", Número de casa: " + n_casa;
+    let direccion = prompt("Ingresa tu dirección: ");
+    let n_casa = parseInt(prompt("Ingresa el número de casa: "));
+    let mensaje = "Dirección: " + direccion + " " + n_casa;
     let longitud = mensaje.length;
     let antepenultimo = mensaje.charAt(longitud - 3);
-    alert("Mensaje: " + mensaje + "\nLongitud: " + longitud + "\nAntepenúltimo carácter: " + antepenultimo);
+    alert("Mensaje: " + mensaje + "\nCantidad de caracteres: " + longitud + "\nAntepenúltimo carácter: " + antepenultimo);
 }
