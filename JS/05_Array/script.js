dbc662fca341565898a379059540ad639b9f5d62
AsioStygius <165288906+DamianOyarceVVH@users.noreply.github.com>
2025-03-19 08:46:29 -0300

    1.3

---
+++ JS/05_Array/script.js
@@ -0,0 +1,4 @@
+
+
+// Ejercicio 6: Producto de Valores Pares en un Arreglo 
+    function Producto_pares() {     let arreglo = [2, 4, 6, 8, 10, 12];     let producto = arreglo[0] * arreglo[1] * arreglo[2] * arreglo[4] * arreglo[5];     alert("El array es: " + arreglo + "\nEl producto de los valores pares es: " + producto); }
