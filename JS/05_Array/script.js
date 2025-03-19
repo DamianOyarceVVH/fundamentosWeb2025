c8c225ecea12c6aaca79978284d095fa41c37871
AsioStygius <damianoyarce@liceovvh.cl>
2025-03-19 09:04:57 -0300

    1.3 fix

--- JS/05_Array/script.js
+++ JS/05_Array/script.js
@@ -1,4 +1,15 @@
+console.log("Vinculado correctamente...")
 
+// Ejercicio 5: Suma de Valores tomando índices de un arreglo
+function sumaIndices() {
+    let arreglo = [1, 2, 3, 4, 5, 6, 7];
+    let suma = arreglo[6] + arreglo[5] + arreglo[0];
+    alert("El arreglo es: " + arreglo + "\nLa suma de los valores es: " + suma);
+}
 
 // Ejercicio 6: Producto de Valores Pares en un Arreglo 
-    function Producto_pares() {     let arreglo = [2, 4, 6, 8, 10, 12];     let producto = arreglo[0] * arreglo[1] * arreglo[2] * arreglo[4] * arreglo[5];     alert("El array es: " + arreglo + "\nEl producto de los valores pares es: " + producto); }
+function productoPares() {
+    let arreglo = [2, 4, 6, 7, 10, 12];
+    let producto = arreglo[0] * arreglo[1] * arreglo[2] * arreglo[4] * arreglo[5];
+    alert("El arreglo es: " + arreglo + "\nEl producto de los valores pares es: " + producto);
+}
