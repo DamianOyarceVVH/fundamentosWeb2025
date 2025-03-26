78f4f59a899c45d3c6f86b487cf36ac6a1c4b1ed
DamianOyarceVVH <damianoyarce@liceovvh.cl>
2025-03-26 09:30:14 -0300

    array fix

--- JS/09_Bucles/script.js
+++ JS/09_Bucles/script.js
@@ -1,33 +1,39 @@
 console.log("Vinculado correctamente...")
 
-function imp_num(){
+function imp_num() {
     let num = 1;
-
+    array = []
     while (num <= 5){
-        alert(num);
+        array.push(num);
         num++;
     }
+    alert(`Los números son:\n ${array.join(" - ")}`)
 }
 
-function imp_num_r(){
+function imp_num_r() {
     let num = 10;
-
-    while (num >= 1){
-        alert(num);
-        num--;
+    let array = [];
+    while (num >= 1) {
+        array.push(num);
+        num--
     }
+    alert(`Los números son:\n ${array.join(" - ")}`)
 }
 
-function sum_num(){
+function sum_num() {
     let suma = 0
-    for(let i = 1; i <= 5; i++){
+    array = []
+    for(let i = 1; i <= 6; i++) {
+        array.push(suma);
         suma += i;
     }
-    alert("Esta es la suma de los primeros 5 números: " + suma)
+    alert(`Esta es la suma de los primeros 5 números:\n ${array.join(" - ")}`)
 }
 
-function imp_mul(){
+function imp_mul() {
+    let array = [];
     for (let i = 2; i <= 10; i += 2) {
-        alert(i);
-    }    
+        array.push(i);
+    }
+    alert(`Estos son los múltiplos de 2 desde el 1 al 10:\n ${array.join(" - ")}`)
 }
