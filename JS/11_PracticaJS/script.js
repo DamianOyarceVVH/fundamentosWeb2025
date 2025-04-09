63f3c4f938b8911ccd4a09a5a881742521c0cb3e
DamianOyarceVVH <damianoyarce@liceovvh.cl>
2025-04-09 16:13:10 -0400

    1.8.2

--- JS/11_PracticaJS/script.js
+++ JS/11_PracticaJS/script.js
@@ -65,14 +65,92 @@ function sumN() {
 }
 
 // Leer 2 números y solicitar al usuario que ingrese la opción de sumarlos, restarlos, multiplicarlos y dividirlos según elija quien ingresa.
+function opciones() {
+    let n1 = parseInt(prompt("Ingrese el primer número de la operación"));
+    let n2 = parseInt(prompt("Ingrese el segundo número de la operación"));
+    let operacion = prompt("¿Qué operación desea realizar?\n+ - * /");
 
+    if (operacion === "+") {
+        alert(`La suma de ambos números (${n1} + ${n2})\nEs: ${n1 + n2}`);
+    } else if (operacion === "-") {
+        alert(`La resta de ambos números (${n1} - ${n2})\nEs: ${n1 - n2}`);
+    } else if (operacion === "*") {
+        alert(`La multiplicación de ambos números (${n1} * ${n2})\nEs: ${n1 * n2}`);
+    } else if (operacion === "/") {
+        if (n2 !== 0) {
+            alert(`La división de ambos números (${n1} / ${n2})\nEs: ${n1 / n2}`);
+        } else {
+            alert("No se puede dividir entre 0");
+        }
+    } else {
+        alert("No se ha elegido una operación válida");
+    }
+}
 // Sumar N números pares.
+function sumarPares() { 
+    let n = parseFloat(prompt("¿Cuántos números deseas sumar?")); 
+    let suma = 0; 
+    let array = [] 
+
+    for (let i = 1; i <= n; i++) { 
+        let numero = parseInt(prompt(`Ingresa el número ${i}:`)); 
+        
+        while (numero % 2 !== 0) { 
+            numero = parseInt(prompt(`El número ingresado es impar. Por favor, ingresa un número par para el número ${i}:`)); 
+        }
+        suma += numero; 
+        array.push(numero);
+    }
+    alert(`La suma total de los siguientes ${n} números pares:\n (${array.join(" - ")})\n Es: ${suma}`); 
+} 
 
 // Sumar los N primeros pares.
+function nPares() { 
+    let n = parseFloat(prompt("¿Cuántos primeros pares desea sumar?")); 
+    let par = 2
+    let suma = 0; 
+    let array = [] 
+
+    for (let i = 1; i <= n; i++) {
+        suma += par;
+        array.push(par);
+        par += 2
+    }
+    alert(`La suma de los primeros ${n} números pares:\n(${array.join(" - ")})\nEs: ${suma}`)
+} 
 
 // Sumar N números impares.
+function sumarImpares() { 
+    let n = parseFloat(prompt("¿Cuántos números deseas sumar?"));
+    let suma = 0; 
+    let array = [] 
+
+    for (let i = 1; i <= n; i++) { 
+        let numero = parseInt(prompt(`Ingresa el número ${i}:`)); 
+        
+        while (numero % 2 === 0) { 
+            numero = parseInt(prompt(`El número ingresado es par. Por favor, ingresa un número impar para el número ${i}:`)); 
+        }
+        suma += numero; 
+        array.push(numero);
+    }
+    alert(`La suma total de los siguientes ${n} números impares:\n (${array.join(" - ")})\n Es: ${suma}`); 
+} 
 
 // Sumar los N primeros impares.
+function nImpares() { 
+    let n = parseFloat(prompt("¿Cuántos primeros impares desea sumar?")); 
+    let impar = 1
+    let suma = 0; 
+    let array = [] 
+
+    for (let i = 1; i <= n; i++) {
+        suma += impar;
+        array.push(impar);
+        impar += 2
+    }
+    alert(`La suma de los primeros ${n} números impares:\n(${array.join(" - ")})\nEs: ${suma}`)
+} 
 
 // Sumar los N primeros múltiplos de 3.
 
