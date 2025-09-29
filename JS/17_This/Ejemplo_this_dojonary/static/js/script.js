996c399f5c54d399080c0e58146446846c6d1419
DamianOyarceVVH <damianoyarce@liceovvh.cl>
2025-09-29 10:19:20 -0300

    small fix

--- JS/17_This/Ejemplo_this_dojonary/static/js/script.js
+++ JS/17_This/Ejemplo_this_dojonary/static/js/script.js
@@ -1,21 +1,22 @@
 let contador1 = 3;
-            let contador2 = 3;
-            function sumarLikes() {
-                alert("This page says\nNinja was liked")
-                document.getElementById('contador-1').textContent = contador1;
-                contador1++;
-            }
+function sumarLikes() {
+    document.getElementById('contador-1').textContent = contador1;
+    contador1++;
+    alert("This page says\nNinja was liked");
+    
+}
 
-            function sumarLikes2() {
-                alert("This page says\nNinja was liked")
-                document.getElementById('contador-2').textContent = contador2;
-                contador2++;
-            }
+let contador2 = 3
+function sumarLikes2() {
+    document.getElementById('contador-2').textContent = contador2;
+    contador2++;
+    alert("This page says\nNinja was liked");
+}
 
-            function login(element) {
-                element.innerText = "Logout";
-            }
+function login(element) {
+    element.innerText = "Logout";
+}
 
-            function hide(element) {
-                element.remove();
-            }
+function hide(element) {
+    element.remove();
+}
